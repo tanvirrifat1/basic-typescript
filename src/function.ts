1e9417091bdfa42aa780eb312d54c850b5d2f723
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-01 22:17:11 +0600

    spreed operator

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,33 @@
-function add(num1:number,num2:number)
+function add(num1:number,num2:number=10)
 :number{
     return num1+num2
 }
+add(24,55)
+
+//spreed operator
+
+const myFriends=['chandir','teli','nani']
+const newFriends=['mocika','konica','sonika']
+const myBestFriend={
+    FullName:'Abesh Rahman',
+    age:18
+}
+
+const [bestFriend]=myFriends
+const{FullName}=myBestFriend
+console.log(FullName);
+
+myFriends.push(...newFriends)
+console.log(myFriends)
+
+//rest paremeter
+
+const greetFriends=(...friends:string[]):void=>
+friends.forEach((friend)=>console.log(`hi ${friend}`))
+
+greetFriends('kasem','abul','kabul','bangla vai','green')
 
 const arrowFun=(num1:number,num2:number):number=>num1+num2
-
 const person:{
     name:string,
     balance:number,
