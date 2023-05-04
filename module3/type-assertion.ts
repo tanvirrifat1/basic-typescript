4e6324469fe7bc07fe73270e61b3d50dd4ca6216
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-04 10:19:47 +0600

    added type-assertion

---
+++ module3/type-assertion.ts
@@ -0,0 +1,22 @@
+function KgToGram(param: string | number): string | number | undefined {
+    if (typeof param === 'string') {
+        const value = parseFloat(param) * 1000
+        return `The converted result is: ${value} gram`
+    }
+    if (typeof param === 'number') {
+        const value = param * 1000
+        return value
+    }
+}
+
+const resultToBeNumber=KgToGram(1000) as number
+
+type CustomError={
+    message:string
+}
+
+try{
+
+}catch(err){
+    console.log((err as CustomError).message);
+}
