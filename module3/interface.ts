764a6d32939891d76dd536568f0826be3ec1ab0b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-04 11:10:36 +0600

    added interface

---
+++ module3/interface.ts
@@ -0,0 +1,21 @@
+type User = {
+  name: string;
+  age: number;
+};
+
+interface IUser {
+  name: string;
+  age: number;
+}
+
+const userWithTypeAlias: User = {
+  name: "type alias",
+  age: 11545,
+};
+
+// typeAlias
+type addNumbersType = (
+  num1: number,
+  num2: boolean
+) => number | string | boolean;
+const addNumbers: addNumbersType = (num1, num2) => num1 + "num2";
