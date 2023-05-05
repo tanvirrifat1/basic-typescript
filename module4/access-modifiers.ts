063fc5598d20b914c036fbe986e7135394f01ef3
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-05 23:54:52 +0600

    added access-modifiers

---
+++ module4/access-modifiers.ts
@@ -0,0 +1,16 @@
+class BankAccount {
+  id: number;
+  name: string;
+  private _balance: number;
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+  getBalance() {
+    console.log(`my current balance is ${this._balance}`);
+  }
+}
+const myAccount = new BankAccount(545, "Hume", 54);
+console.log(myAccount);
