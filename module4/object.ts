b017373d3676ac917ae3e0b132a4eb44cb50f2f6
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-05 21:36:48 +0600

    added object ts

---
+++ module4/object.ts
@@ -0,0 +1,19 @@
+class Animal {
+  name: string;
+  //   species: string;
+  //   sound: string;
+
+  constructor(name: string, public species: string, public sound: string) {
+    this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+  }
+  makeSound() {
+    console.log(`The ${this.name} says ${this.sound}`);
+  }
+}
+
+const dog = new Animal("german shapard", "dog", "ghew ghew");
+const cat = new Animal("Persian", "dog", "meaw meaw");
+dog.makeSound();
+cat.makeSound();
