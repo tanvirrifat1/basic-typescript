b5d0b5a748967bb46e2da4fcb79b9d08be72ed2e
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-05 20:56:56 +0600

    added map-type

---
+++ module3/mapped-type.ts
@@ -0,0 +1,13 @@
+const arrayOfNumbers = [1, 2, 3, 4];
+const arrayOfStrings = arrayOfNumbers.map((number) => number.toString());
+console.log(arrayOfStrings);
+
+type volume = {
+  height: number;
+  width: number;
+  depth: number;
+};
+
+type Area = {
+  [key in keyof volume]: string;
+};
