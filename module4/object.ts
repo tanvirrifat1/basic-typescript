2e39e823ed9e5bcdcc2dc4848d8c2a8909637aaf
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-05 23:37:00 +0600

    added type-guard

--- module4/object.ts
+++ module4/object.ts
@@ -1,19 +1,19 @@
-class Animal {
-  name: string;
-  //   species: string;
-  //   sound: string;
+// class Animal {
+//   name: string;
+//   //   species: string;
+//   //   sound: string;
 
-  constructor(name: string, public species: string, public sound: string) {
-    this.name = name;
-    // this.species = species;
-    // this.sound = sound;
-  }
-  makeSound() {
-    console.log(`The ${this.name} says ${this.sound}`);
-  }
-}
+//   constructor(name: string, public species: string, public sound: string) {
+//     this.name = name;
+//     // this.species = species;
+//     // this.sound = sound;
+//   }
+//   makeSound() {
+//     console.log(`The ${this.name} says ${this.sound}`);
+//   }
+// }
 
-const dog = new Animal("german shapard", "dog", "ghew ghew");
-const cat = new Animal("Persian", "dog", "meaw meaw");
-dog.makeSound();
-cat.makeSound();
+// const dog = new Animal("german shapard", "dog", "ghew ghew");
+// const cat = new Animal("Persian", "dog", "meaw meaw");
+// dog.makeSound();
+// cat.makeSound();
