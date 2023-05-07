6886b32ff94b58483a1ef6e15f86cac75f70d03e
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-07 14:46:02 +0600

    added static ts

--- module4/type-guard.ts
+++ module4/type-guard.ts
@@ -1,14 +1,14 @@
 type Alpha = string | number;
 
-function add(param1: Alpha, param2: Alpha): Alpha {
+function addd(param1: Alpha, param2: Alpha): Alpha {
   if (typeof param1 === "number" && typeof param2 === "number") {
     return param1 + param2;
   } else {
     return param1.toString() + param2.toString();
   }
 }
-add("1", "2");
-add(2, 4);
+addd("1", "2");
+addd(2, 4);
 
 // in guard
 
