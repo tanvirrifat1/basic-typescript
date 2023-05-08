4595a2effcebc32fbd26ca811e4ec194b3be4a61
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-08 22:30:30 +0600

    added type_alias

--- Extra_practice_module/function.ts
+++ Extra_practice_module/function.ts
@@ -1,26 +1,42 @@
-// normal function
-function add(num1: number, num2: string): number | string {
-  return num1 + parseFloat(num2);
-}
-console.log(add(55, "4544"));
+// // normal function
+// function add(num1: string, num2: number = 2) {
+//   console.log(num1.repeat(num2));
+// }
+// // console.log(add("hello"));
+// // spread operator
 
-// array function
-const AddArrow = (num1: number, num2: number): number => num1 + num2;
-console.log(AddArrow(12, 42));
+// const frnd = ["hello", "mello", "gello"];
+// const newFrnd = ["tello", "sello", "pello"];
 
-const arr = [2, 5, 8];
+// frnd.push(...newFrnd);
+// // console.log(frnd);
 
-const newArr = arr.map((el: number) => el * el);
-console.log(newArr);
+// // array function
+// const AddArrow = (num1: number, num2: number): number => num1 + num2;
+// // console.log(AddArrow(12, 42));
 
-const person: {
-  name: string;
-  balance: number;
-  addBalance(money: number): string;
-} = {
-  name: "abul",
-  balance: 5,
-  addBalance(money: number) {
-    return `My total Balance ${this.balance + money}`;
-  },
-};
+// const arr = [2, 5, 8];
+
+// const newArr = arr.map((el: number) => el * el);
+// // console.log(newArr);
+
+// const person: {
+//   name: string;
+//   balance: number;
+//   addBalance(money: number): string;
+// } = {
+//   name: "abul",
+//   balance: 5,
+//   addBalance(money: number) {
+//     return `My total Balance ${this.balance + money}`;
+//   },
+// };
+
+// // rest operator
+// // const greetFriends = (frnd1: string, frnd2: string, frnd3: string): void =>
+// //   console.log(` hi ${frnd1} \n hi ${frnd2} \n hi ${frnd3}`);
+
+// const grtFriends = (...friends: string[]): void =>
+//   friends.forEach((frnds) => console.log(`hi ${frnds}`));
+
+// grtFriends("abul", "sabul", "tabul", "bangla vai", "dfhgksdhf");
