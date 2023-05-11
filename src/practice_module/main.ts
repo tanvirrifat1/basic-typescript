9eccb1b3c8cfe9e0b89c58d45535fb5af6fc7b94
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-11 22:02:18 +0600

    added more

--- src/practice_module/main.ts
+++ src/practice_module/main.ts
@@ -1,8 +1,8 @@
-import { add as addTwo } from "./module";
+import any from "./module";
 
 const add = (param1: number, param2: number, param3: number): number => {
   return param1 + param2 + param3;
 };
 
-const res = addTwo(4, 8);
+const res = any.multiply(10, 2);
 console.log(res);
--- src/practice_module/module.ts
+++ src/practice_module/module.ts
@@ -1,7 +1,21 @@
-export const add = (param1: number, param2: number): number => {
+const add = (param1: number, param2: number): number => {
   return param1 + param2;
 };
 
-export const subtract = (param1: number, param2: number): number => {
+const subtract = (param1: number, param2: number): number => {
   return param1 - param2;
+};
+const multiply = (param1: number, param2: number): number => {
+  return param1 * param2;
+};
+
+const divide = (param1: number, param2: number): number => {
+  return param1 / param2;
+};
+
+export default {
+  add,
+  subtract,
+  multiply,
+  divide,
 };
